b8877b678847ea1c5cfcb58a10cb6217c28d163f
Junior Chagas <jjuniorchagas15@gmail.com>
2021-03-11 18:42:53 -0300

    initi repo

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import { View, Text } from 'react-native'; 
+
+
+const App = () => {
+  return(
+    <View style={{justifyContent:'center', alignItems:'center'}}>
+      <Text>Olá Mundo!</Text>
+    </View>
+  ); 
+}; 
+
+export default App; 
